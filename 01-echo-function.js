fe3e1998ddae926ca38ed8e629e3eaf2d3b1d4e2
NGNikolay <georgiev.nikolaj@gmail.com>
2023-04-15 13:15:56 +0300

    added solutions

---
+++ 01-echo-function.js
@@ -0,0 +1,7 @@
+function echo(string) {
+    console.log(string.length);
+    console.log(string);
+}
+
+echo('Hello, JavaScript!');
+echo('strings are easy');
---
+++ 02-string-length.js
@@ -0,0 +1,10 @@
+function stringLength(firstString, secondString, thirdString) {
+
+    let resultLength = (firstString + secondString + thirdString).length;
+    console.log(resultLength);
+    console.log(resultLength / 3 | 0);
+
+}
+
+stringLength('chocolate', 'ice cream', 'cake');
+stringLength('pasta', '5', '22.3');
