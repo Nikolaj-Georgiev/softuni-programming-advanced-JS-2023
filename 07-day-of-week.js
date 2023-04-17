729f8bb333a8a2c11f026dddb1748b3b8f39cceb
Nikolay Georgiev <47087200+NGNikolay@users.noreply.github.com>
2023-04-17 13:51:44 +0300

    Update 07-day-of-week.js

--- 07-day-of-week.js
+++ 07-day-of-week.js
@@ -1,13 +1,32 @@
 function dayOfWeek(string) {
-    let days = ['hui', 'Monday', 'Tuesday', 'Wednesday', 'Thirsday', 'Friday', 'Saturday', 'Sunday'];
+    let days = ['hui', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     for (let i = 0; i < days.length; i++) {
         if (days[i] === string) {
             return i;
         }
     }
-    return 'error'
+    return 'error';
 }
 
-console.log(dayOfWeek('Thirsday'));
-// dayOfWeek('Friday');
-// dayOfWeek('Invalid');
+console.log(dayOfWeek('Thursday'));
+dayOfWeek('Friday');
+dayOfWeek('Invalid');
+
+function dayOfWeek1(param) {
+    let days = ['hui', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    let result = 0;
+    days.forEach((x, i) => {
+        if (x === param) {
+            result = i;
+            return result;
+        }
+    });
+    if (result) {
+        return result;
+    } else {
+        return 'error';
+    }
+}
+
+
+console.log(dayOfWeek1('Thursday'));
