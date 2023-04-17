d5db9ba4e727c2834309ce2854b63f3750a0bd90
Nikolay Georgiev <47087200+NGNikolay@users.noreply.github.com>
2023-04-17 12:57:08 +0300

    Update 03-largest-number.js

--- 03-largest-number.js
+++ 03-largest-number.js
@@ -4,3 +4,20 @@ function largestNumber(...n) {
 
 largestNumber(5, -3, 16);
 largestNumber(-3, -5, -22.5);
+
+function lN(n1, n2, n3) {
+    let result;
+    if (n1 > n2 && n1 > n3) {
+        result = n1;
+    } else if(n2 > n1 && n2 > n3) {
+        result = n2;
+    } else {
+        result = n3;
+    }
+
+    console.log(`The largest number is ${result}.`);
+
+}
+
+lN(5, -3, 16)
+lN(-3, -5, -22.5)
