e3203860238ea5b319886c49cdeb6bf52a3cce02
Nikolay Georgiev <47087200+NGNikolay@users.noreply.github.com>
2023-04-17 13:13:42 +0300

    Create 04-circle-area.js

---
+++ 04-circle-area.js
@@ -0,0 +1,12 @@
+function circleArea(arg) {
+
+    if (typeof arg === 'number') {
+        let result = Math.PI * (arg**2);
+        return Number(result.toFixed(2));
+    } else {
+        return `We can not calculate the circle area, because we receive a ${typeof arg}.`;
+    }
+}
+
+console.log(circleArea(5));
+console.log(circleArea('name'));
