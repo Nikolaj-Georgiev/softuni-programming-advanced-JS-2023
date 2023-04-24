374177b95de4b9c79c851a1775147af313ff1a18
NGNikolay <georgiev.nikolaj@gmail.com>
2023-04-24 20:16:08 +0300

    Create 03-same-numbers.js

---
+++ syntax-functions-and-statements/exercise/03-same-numbers.js
@@ -0,0 +1,12 @@
+function sameNumbers(number) {
+    let numAsArr = number.toString().split('').map(Number);
+    let lenChecker = numAsArr.length;
+    let checkedArr = numAsArr.filter(x => x === numAsArr[0]);
+    let sumOfDigits = numAsArr.reduce((a, c) => a + c, 0);
+    lenChecker === checkedArr.length ? console.log(true) : console.log(false);
+    console.log(sumOfDigits);
+
+}
+
+sameNumbers(2222222);
+sameNumbers(1234);
