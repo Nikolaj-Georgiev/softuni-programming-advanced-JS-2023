2d68b8c2b8b9567be5be42b8275986baefe7a690
NGNikolay <georgiev.nikolaj@gmail.com>
2023-04-29 16:10:47 +0300

    Create 09-biggest-element.js

---
+++ arrays-and-nested-arrays/09-biggest-element.js
@@ -0,0 +1,61 @@
+// function biggestElement(matrix) {
+
+//     let biggestElement;
+
+//     for (let i = 0; i < matrix.length; i++) {
+//         for (let j = 0; j < matrix[i].length; j++) {
+//             if (biggestElement) {
+//                 if (biggestElement < matrix[i][j]) {
+//                     biggestElement = matrix[i][j];
+//                 }
+//             } else {
+//                 biggestElement = matrix[i][j];
+//             }
+//         }
+//     }
+
+//     return biggestElement;
+// }
+// const test1 = biggestElement([
+//     [],
+//     []
+// ]);
+// const test2 = biggestElement([
+//     [3, 5, 7, 12],
+//     [-1, 4, 33, 2],
+//     [8, 3, 0, 4]
+// ]);
+
+// console.log(test1);
+// console.log(test2);
+
+function biggestElement1(matrix) {
+
+    let biggestElement;
+
+    matrix.forEach(arr => {
+        let biggest = Math.max(...arr);
+        if (biggestElement) {
+            if (biggest > biggestElement) {
+                biggestElement = biggest;
+            }
+        } else {
+            biggestElement = biggest;
+        }
+    });
+
+    return biggestElement;
+}
+
+const test3 = biggestElement1([
+    [],
+    []
+]);
+const test4 = biggestElement1([
+    [3, 5, 7, 12],
+    [-1, 4, 33, 2],
+    [8, 3, 0, 4]
+]);
+
+console.log(test3);
+console.log(test4);
