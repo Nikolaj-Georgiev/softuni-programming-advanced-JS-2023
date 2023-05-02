04c6fd2814798aa76e08a4cd5a0f4bb7e48dc5ca
NGNikolay <georgiev.nikolaj@gmail.com>
2023-05-02 16:16:04 +0300

    Update 08-sort-an-array-by-2-criteria.js

--- arrays-and-nested-arrays/exercise/08-sort-an-array-by-2-criteria.js
+++ arrays-and-nested-arrays/exercise/08-sort-an-array-by-2-criteria.js
@@ -9,3 +9,17 @@ console.log('++++++++++++++++++');
 console.log(sortArrayByTwoCriteria(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']));
 console.log('++++++++++++++++++');
 console.log(sortArrayByTwoCriteria(['test', 'Deny', 'omen', 'Default']));
+
+function sortArrayByTwoCriteria2(input) {
+    return input.sort(twoCriteriaSorting).join('\n');
+
+    function twoCriteriaSorting(current, next) {
+        if (current.length === next.length) {
+            return current.localeCompare(next);
+        }
+        return current.length - next.length;
+    }
+}
+
+console.log('++++++++++++++++++');
+console.log(sortArrayByTwoCriteria2(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']));
