76b30b16cab8e698d42d69f91d4f93cb8d9893c9
NGNikolay <georgiev.nikolaj@gmail.com>
2023-05-05 11:39:58 +0300

    Create 01-city-record.js

---
+++ objects-and-composition/01-city-record.js
@@ -0,0 +1,21 @@
+function cityRecord(name, population, treasury) {
+     const city = {
+        name,
+        population,
+        treasury
+    };
+    return city;
+
+}
+
+let test1 = cityRecord('Tortuga', 7000, 15000);
+let test2 = cityRecord('Santo Domingo', 12000, 23500);
+
+console.log(test1.treasury);
+console.log(test2);
+
+test1.weather = 'Sunny';
+
+console.log(test1.weather);
+console.log(test1.weather = 'nice');
+console.log(test1);
