7677c256552b79152d43ca3f46205d08158ad8f4
NGNikolay <georgiev.nikolaj@gmail.com>
2023-05-12 14:06:35 +0300

    Update subtract.js

--- DOM-introduction/exercise/01. Subtraction/subtract.js
+++ DOM-introduction/exercise/01. Subtraction/subtract.js
@@ -1,3 +1,6 @@
 function subtract() {
-    console.log('TODO:...');
+    let firstNumber = +document.getElementById('firstNumber').value;
+    let secondNumber = +document.getElementById('secondNumber').value;
+    let result = firstNumber - secondNumber;
+    document.getElementById('result').textContent = result;
 }
