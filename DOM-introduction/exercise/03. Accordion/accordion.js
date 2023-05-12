5ef330b609442c4337936e232d836192e5eaacf6
NGNikolay <georgiev.nikolaj@gmail.com>
2023-05-12 15:13:45 +0300

    Update accordion.js

--- DOM-introduction/exercise/03. Accordion/accordion.js
+++ DOM-introduction/exercise/03. Accordion/accordion.js
@@ -1,3 +1,20 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonText = document.getElementById('extra').style.display;
+    if (buttonText === '') {
+        buttonText = 'none'
+    };
+    buttonChecker(buttonText);
+    function buttonChecker(buttonText) {
+        if (buttonText === 'none') {
+            document.getElementById('extra').style.display = 'block';
+            document.querySelector('#accordion .button').textContent = 'Less';
+            return ;        
+        }
+
+        if (buttonText !== 'none') {
+            document.getElementById('extra').style.display = 'none';
+            document.querySelector('#accordion .button').textContent = 'More';
+            return ;
+        }
+    }
 }
