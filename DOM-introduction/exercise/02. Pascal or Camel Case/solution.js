cf76ba7e10909807d844d74c7f00c68f65444943
NGNikolay <georgiev.nikolaj@gmail.com>
2023-05-13 10:40:53 +0300

    Update solution.js

--- DOM-introduction/exercise/02. Pascal or Camel Case/solution.js
+++ DOM-introduction/exercise/02. Pascal or Camel Case/solution.js
@@ -18,3 +18,21 @@ function solve() {
   }
   document.getElementById('result').textContent = result;
 }
+
+// function solve() {
+//   const text = document.getElementById('text').value;
+//   const convention = document.getElementById('naming-convention').value;
+//   let result = '';
+//   result = text
+//       .split(' ')
+//       .map((w, i) => i === 0 ? w.toLowerCase() : w.slice(0, 1).toUpperCase() + w.slice(1).toLowerCase())
+//       .join('');
+//   if (convention === 'Camel Case') {
+//     return document.getElementById('result').textContent = result;
+//   } else if (convention === 'Pascal Case') {
+//     result = result.slice(0, 1).toLocaleUpperCase() + result.slice(1);
+//   } else {
+//     result = 'Error!';
+//   }
+//   document.getElementById('result').textContent = result;
+// }
