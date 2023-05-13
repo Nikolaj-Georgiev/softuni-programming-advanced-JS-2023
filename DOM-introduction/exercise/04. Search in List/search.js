bfa3ae8d3b8f6aebe359c6caed454e3cec5ca33c
NGNikolay <georgiev.nikolaj@gmail.com>
2023-05-13 10:41:00 +0300

    Update search.js

--- DOM-introduction/exercise/04. Search in List/search.js
+++ DOM-introduction/exercise/04. Search in List/search.js
@@ -1,3 +1,18 @@
 function search() {
-   // TODO
+   // const townsLi = document.getElementsByTagName('ul')[0].children;
+   const townsLi = document.querySelectorAll('ul#towns li');
+   let search = document.getElementById('searchText').value;
+   let matches = 0;
+
+   for (let element of townsLi) {
+      element.style.fontWeight = 'normal';
+      element.style.textDecoration = '';
+      let text = element.textContent;
+      if (text.match(search)) {
+         matches++;
+         element.style.fontWeight = 'bold';
+         element.style.textDecoration = 'underline';
+      }
+   }
+   let resultMatches = document.getElementById('result').textContent = `${matches} matches found`;
 }
