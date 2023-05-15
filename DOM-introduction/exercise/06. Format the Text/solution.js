8fea64e61c995c1aaa9663ae067b9d135843e3fa
NGNikolay <georgiev.nikolaj@gmail.com>
2023-05-15 12:07:52 +0300

    Update solution.js

--- DOM-introduction/exercise/06. Format the Text/solution.js
+++ DOM-introduction/exercise/06. Format the Text/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-  //TODO
+  let text = document.querySelector('#exercise textarea');
+  let sentences = text.value.split('.').filter(s => s !== '').filter(s => s != '\n');
+  console.log(sentences);
+  let output = document.querySelector('div#output');
+ 
+  while (sentences.length > 0) {   
+    let p = document.createElement('p');
+    output.appendChild(p);
+    p.textContent = sentences.splice(0, 3) + '.';
+  }
 }
