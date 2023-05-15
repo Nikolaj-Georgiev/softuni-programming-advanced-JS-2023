9ffd1b6040bed76d06ec5cd513e3a9cefe6b5ffb
NGNikolay <georgiev.nikolaj@gmail.com>
2023-05-15 11:02:41 +0300

    Update solution.js

--- DOM-introduction/exercise/05. Table - Search Engine/solution.js
+++ DOM-introduction/exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,18 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
-   function onClick() {
-      //   TODO:
+      let rows = document.querySelectorAll('tbody tr');
+      let input = document.getElementById('searchField');
 
+   function onClick() {
+
+      for (const row of rows) {
+         row.className = '';
+         if (row.textContent.includes(input.value) && input.value !== '') {
+            row.className = 'select';
+            console.log(row.className);
+         }
+      }
+      input.value = '';
    }
 }
