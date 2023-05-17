08cd86a4194c328a8015d70e4d0821e07eaf26ea
NGNikolay <georgiev.nikolaj@gmail.com>
2023-05-17 13:30:05 +0300

    Update report.js

--- DOM-introduction/exercise/08. Generate Report/report.js
+++ DOM-introduction/exercise/08. Generate Report/report.js
@@ -1,3 +1,21 @@
 function generateReport() {
-    //TODO
+
+    const headers = Array.from(document.querySelectorAll('thead tr th'));
+    const tableRows = Array.from(document.querySelectorAll('tbody tr'));
+    const result = [];
+
+    tableRows.forEach(row => result.push(objectCreator(row.children, headers)));
+
+    function objectCreator(rowArray, headersArr) {
+        const localObject = {};
+        headersArr.forEach((head, i) => {
+            if (head.children[0].checked) {
+                localObject[head.lastChild.name] = rowArray[i].textContent;
+            }
+        });
+        return localObject;
+    }
+
+    const reportOutput = document.querySelector('div textarea#output');
+    reportOutput.textContent = JSON.stringify(result, null, 2);
 }
