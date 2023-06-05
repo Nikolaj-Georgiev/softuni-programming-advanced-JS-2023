d1713047a64b204230fd33199fe2a22df04c0cbd
NGNikolay <georgiev.nikolaj@gmail.com>
2023-06-05 10:56:58 +0300

    Update 01-sub-sum.js

--- unit-testing-and-error-handling/lab/01-sub-sum.js
+++ unit-testing-and-error-handling/lab/01-sub-sum.js
@@ -1,17 +1,11 @@
 function subSum(arr, start, end) {
     let sum = 0;
-
     if (!Array.isArray(arr)) {
         return NaN;
     }
 
-    if (start < 0) {
-        start = 0;
-    }
-
-    if (end > arr.length - 1) {
-        end = arr.length -1;
-    }
+    (start < 0)?start = 0: start;
+    (end > arr.length - 1)? end = arr.length - 1: end;
 
     for (let i = start; i <= end; i++) {
         sum += Number(arr[i]);
