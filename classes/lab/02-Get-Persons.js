fb8596b90846712d569f8a12f008bcd79094b095
NGNikolay <georgiev.nikolaj@gmail.com>
2023-06-11 16:31:31 +0300

    Create 02-Get-Persons.js

---
+++ classes/lab/02-Get-Persons.js
@@ -0,0 +1,28 @@
+// const Person = require('./01-Person');
+function wrapper() {
+    class Person {
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+        }
+
+    }
+
+    const person1 = new Person('Anna', 'Simpson', 22, 'anna@yahoo.com');
+    const person2 = new Person('SoftUni');
+    const person3 = new Person('Stephan', 'Johnson', 25);
+    const person4 = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
+
+    return [person1, person2, person3, person4];
+}
+
+console.log(wrapper().join('\n'))
+
+
+
