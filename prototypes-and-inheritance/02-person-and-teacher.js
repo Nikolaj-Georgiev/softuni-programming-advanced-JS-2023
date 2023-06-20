72d6122f3719fcf41edbb16e3d3689b9b61c8362
NGNikolay <georgiev.nikolaj@gmail.com>
2023-06-20 11:36:45 +0300

    Create 02-person-and-teacher.js

---
+++ prototypes-and-inheritance/02-person-and-teacher.js
@@ -0,0 +1,52 @@
+// class Person{
+//     constructor(name, email){
+//         this.name = name;
+//         this.email = email;
+//     }
+// }
+
+// class Teacher extends Person{
+//     constructor(name, email, subject){
+//         super(name, email);
+//         this.subject = subject;
+//     }
+// }
+
+// const p = new Person('Pehso', 'gbg@abv.bg');
+// console.log(p);
+// const t = new Teacher('Slon', 'banan', 'Babalugaaa');
+// console.log(t);
+
+
+function personAndTeacher(){
+    class Person{
+        constructor(name, email){
+            this.name = name;
+            this.email = email;
+        }
+
+        saiHello(){
+            return `${this.name} says hello`;
+        }
+    };
+
+    class Teacher extends Person{
+        constructor(name, email, subject){
+            super(name, email);
+            this.subject = subject;
+        }
+    }
+    const result = {
+        Person,
+        Teacher
+    }
+
+    return result;
+}
+
+let test = personAndTeacher();
+let obj = new test.Person('Pesho', 'asd@abv.bg');
+let obj1 = new test.Teacher('Gosho', 'asd@abv.bg', 'babun');
+
+console.log(obj.saiHello());
+console.log(obj1.saiHello());
