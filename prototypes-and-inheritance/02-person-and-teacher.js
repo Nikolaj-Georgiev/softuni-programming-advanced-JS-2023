c72f585776be48cb3ce7d96eb59dfc3f893614ad
NGNikolay <georgiev.nikolaj@gmail.com>
2023-06-21 19:42:55 +0300

    Update 02-person-and-teacher.js

--- prototypes-and-inheritance/02-person-and-teacher.js
+++ prototypes-and-inheritance/02-person-and-teacher.js
@@ -1,23 +1,3 @@
-// class Person{
-//     constructor(name, email){
-//         this.name = name;
-//         this.email = email;
-//     }
-// }
-
-// class Teacher extends Person{
-//     constructor(name, email, subject){
-//         super(name, email);
-//         this.subject = subject;
-//     }
-// }
-
-// const p = new Person('Pehso', 'gbg@abv.bg');
-// console.log(p);
-// const t = new Teacher('Slon', 'banan', 'Babalugaaa');
-// console.log(t);
-
-
 function personAndTeacher(){
     class Person{
         constructor(name, email){
